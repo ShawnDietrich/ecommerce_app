02ef1d2c6d78ccc0c4f8443804ef135f534dda9b
Shawn Dietrich <shawn.m.dietrich@gmail.com>
2022-05-21 13:18:29 -0400

    added query for products and cart

--- server/models/cartmodel.js
+++ server/models/cartmodel.js
@@ -1,7 +1,50 @@
+const db = require('../../database')
+const pgp = require('pg-promise')
+const table = 'cart'
 
-//modules that will query the database
-//called from services
-
+/**
+ * Collection of methods to query the database
+ * Called from the service
+ */
 module.exports = class CartModel {
-    
+      //get all products from database
+  async getAllProducts() {
+    try {
+      //setup query
+      const query = 'SELECT * FROM products'
+      //query database
+      const result = await db.query(query)
+
+      //Check result
+      if (result) return result.rows[0]
+      return null
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  //Add Product to database
+  async addProduct(data) {
+    try{
+        //setup query
+        const query = pgp.helpers.insert(data, null, table) + 'RETURNING *'
+        //query database
+        const result = db.query(query)
+        if(result) return result.rows[0]
+        return null
+    }catch (err) {
+        throw new Error(err)
+    }
+  }
+
+  async updateProduct(data) {
+      try {
+          const query = pgp.helpers.update(data, null, table) + 'RETURNING *'
+          const result = db.query(query)
+          if (result) return result.rows[0]
+          return null
+      }catch(err) {
+          throw new Error(err)
+      }
+  }
 }
--- server/models/productsmodel.js
+++ server/models/productsmodel.js
@@ -1,10 +1,49 @@
-
-
+const db = require('../../database')
+const pgp = require('pg-promise')()
+const table = 'products'
 /**
  * Collection of methods to query the database
  * Called from the service
  */
 module.exports = class ProductModel {
+  //get all products from database
+  async getAllProducts() {
+    try {
+      //setup query
+      const query = 'SELECT * FROM products'
+      //query database
+      const result = await db.query(query)
 
+      //Check result
+      if (result) return result.rows[0]
+      return null
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
 
+  //Add Product to database
+  async addProduct(data) {
+    try{
+        //setup query
+        const query = pgp.helpers.insert(data, null, table) + 'RETURNING *'
+        //query database
+        const result = db.query(query)
+        if(result) return result.rows[0]
+        return null
+    }catch (err) {
+        throw new Error(err)
+    }
+  }
+
+  async updateProduct(data) {
+      try {
+          const query = pgp.helpers.update(data, null, table) + 'RETURNING *'
+          const result = db.query(query)
+          if (result) return result.rows[0]
+          return null
+      }catch(err) {
+          throw new Error(err)
+      }
+  }
 }
