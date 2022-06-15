99c8a781cddfe4fb21cea3351b4ac2bf92554b36
Shawn Dietrich <shawn.m.dietrich@gmail.com>
2022-06-15 18:34:28 -0400

    added build folder

---
+++ Client/build/precache-manifest.4f972df8c781e367353dca68340003f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "52cd5b60d361a57de964",
+    "url": "/static/css/main.6c2e5e89.chunk.css"
+  },
+  {
+    "revision": "52cd5b60d361a57de964",
+    "url": "/static/js/main.52cd5b60.chunk.js"
+  },
+  {
+    "revision": "ac85486b0534ed24fc07",
+    "url": "/static/js/1.ac85486b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e10f42e032397d302277b9d33edfff44",
+    "url": "/static/media/NoImage.e10f42e0.png"
+  },
+  {
+    "revision": "2daee2c9fa8102d0583df3104993e90d",
+    "url": "/static/media/wedding-rings.2daee2c9.png"
+  },
+  {
+    "revision": "caba61595ddce3687e9b8d904cdbb8b8",
+    "url": "/static/media/bowl.caba6159.png"
+  },
+  {
+    "revision": "f994b7dd10c64a72e8615aadf11fce41",
+    "url": "/index.html"
+  }
+];
